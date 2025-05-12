fa366008866eacbc6e8a49173f1e0ebc786d630c
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2025-05-12 19:49:03 +0800

    Fix build.gradle.kts hard-coded versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,12 @@ repositories {
 }
 
 dependencies {
+	val jjwtVersion = "0.12.6"
+	val expiringMapVersion = "0.5.11"
+	val bucket4JVersion = "8.14.0"
+	val testContainersVersion = "1.21.0"
+	val apacheCommonsLangVersion = "3.17.0"
+
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
@@ -35,20 +41,20 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-mysql")
-	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-	implementation("net.jodah:expiringmap:0.5.11")
-	implementation("com.bucket4j:bucket4j_jdk17-core:8.14.0")
+	implementation("io.jsonwebtoken:jjwt-api:${jjwtVersion}")
+	implementation("net.jodah:expiringmap:${expiringMapVersion}")
+	implementation("com.bucket4j:bucket4j_jdk17-core:${bucket4JVersion}")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:${jjwtVersion}")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:${jjwtVersion}")
 	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation("org.testcontainers:junit-jupiter:1.21.0")
-	testImplementation("org.testcontainers:mariadb:1.21.0")
-	testImplementation("org.apache.commons:commons-lang3:3.17.0")
+	testImplementation("org.testcontainers:junit-jupiter:${testContainersVersion}")
+	testImplementation("org.testcontainers:mariadb:${testContainersVersion}")
+	testImplementation("org.apache.commons:commons-lang3:${apacheCommonsLangVersion}")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
