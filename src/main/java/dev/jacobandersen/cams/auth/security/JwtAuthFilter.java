048d07c639f45872d1262fd159318c842e182994
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2025-05-15 08:54:42 +0800

    Use superclass logger

--- src/main/java/dev/jacobandersen/cams/auth/security/JwtAuthFilter.java
+++ src/main/java/dev/jacobandersen/cams/auth/security/JwtAuthFilter.java
@@ -6,8 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContext;
@@ -20,8 +18,6 @@ import java.util.Arrays;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
-    private static final Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
-
     private final JwtAuthenticator authenticator;
 
     @Autowired
