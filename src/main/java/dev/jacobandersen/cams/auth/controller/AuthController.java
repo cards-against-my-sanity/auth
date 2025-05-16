4294c0035bd82dcdac309447ded68c18281d3203
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2025-05-16 22:08:26 +0800

    Cleanup

--- src/main/java/dev/jacobandersen/cams/auth/controller/AuthController.java
+++ src/main/java/dev/jacobandersen/cams/auth/controller/AuthController.java
@@ -31,7 +31,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Objects;
 import java.util.UUID;
 
 @RestController
