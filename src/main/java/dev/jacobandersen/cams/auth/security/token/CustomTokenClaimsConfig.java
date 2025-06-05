385d9f3fc08850b822c082889af515dce7af2a65
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2025-06-05 20:51:39 +0800

    Add custom claims to access token

--- src/main/java/dev/jacobandersen/cams/auth/security/token/CustomTokenClaimsConfig.java
+++ src/main/java/dev/jacobandersen/cams/auth/security/token/CustomTokenClaimsConfig.java
@@ -1,18 +1,20 @@
-package dev.jacobandersen.cams.auth.security;
+package dev.jacobandersen.cams.auth.security.token;
 
 import dev.jacobandersen.cams.auth.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
 @Configuration
-public class IdTokenConfig {
+public class CustomTokenClaimsConfig {
     @Bean
     public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(UserService userService) {
         return context -> {
-            if (OidcParameterNames.ID_TOKEN.equals(context.getTokenType().getValue())) {
+            final var tokenType = context.getTokenType();
+            if (OAuth2TokenType.ACCESS_TOKEN.equals(tokenType) || OidcParameterNames.ID_TOKEN.equals(tokenType.getValue())) {
                 userService.loadOidcUserInfoByEmail(context.getPrincipal().getName())
                         .ifPresent(oidcUserInfo -> context.getClaims()
                                 .claims(claims -> claims.putAll(oidcUserInfo.getClaims())));
